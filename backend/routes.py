01f9c98f80f4a90379e6a13ec0cf7b1eff3cd93e
Imaad Junaidi <imaadjunaidi1011@gmail.com>
2023-01-21 17:11:51 -0800

    openai image generation working

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,14 @@
 from backend import app
-from flask import render_template, Response
+from flask import render_template, request
+import openai
+
+openai.api_key = app.config['OPENAI_API_KEY']
+
+def createImageFromPrompt(prompt):
+    response = openai.Image.create(prompt=prompt, n=1, size="512x512")
+    return response['data'][0]
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    image = createImageFromPrompt('four people working on a hackathon project')
+    return render_template('index.html', image=image)
