b327f149600ae5dd9cb46f8b7af6376dd983a68b
Imaad Junaidi <imaadjunaidi1011@gmail.com>
2023-01-21 20:51:00 -0800

    added loading stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,76 @@
-import "./App.css";
-import { useEffect, useState } from "react";
+// import "./App.css";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+
+// function App() {
+//   const [picture, setPicture] = useState("");
+
+//   useEffect(() => {
+//     console.log(picture);
+//   }, [picture]);
+
+//   return (
+//     <div>
+//       <h1 class="text-purple-500 justify-center">Piclingo</h1>
+//       <img alt="OpenAI!" src={picture} />
+//       <input />
+//       <button
+//         onClick={() => {
+//           axios.get("http://127.0.0.1:5000/").then(
+//             (response) => {
+//               console.log(response.data);
+//               setPicture(response.data);
+//             },
+//             (error) => {
+//               console.log(error);
+//             }
+//           );
+//         }}
+//       >
+//         Generate
+//       </button>
+//       <button
+//         onClick={() => {
+//           console.log(picture);
+//         }}
+//       >
+//         Test
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+import { useState } from "react";
 import axios from "axios";
 
 function App() {
   const [picture, setPicture] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    console.log(picture);
-  }, [picture]);
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("http://localhost:5000/");
+      console.log(response.data);
+      setPicture(response.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
       <h1 class="text-purple-500 justify-center">Piclingo</h1>
-      <img alt="OpenAI!" src={picture} />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img alt="OpenAI!" src={picture} />
+      )}
       <input />
-      <button
-        onClick={() => {
-          axios.get("http://127.0.0.1:5000/").then(
-            (response) => {
-              console.log(response.data);
-              setPicture(response.data);
-            },
-            (error) => {
-              console.log(error);
-            }
-          );
-        }}
-      >
-        Generate
-      </button>
-      <button
-        onClick={() => {
-          console.log(picture);
-        }}
-      >
-        Test
-      </button>
+      <button onClick={handleClick}>Generate</button>
     </div>
   );
 }
