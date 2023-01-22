d62c887b0e1519b4b4f620e0f177e2fd4ef24d22
Oweang <wangowen02@gmail.com>
2023-01-22 11:36:23 -0800

    frontend changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import { useState } from "react";
 import axios from "axios";
 
@@ -9,6 +10,9 @@ function App() {
   const handleClick = async () => {
     setLoading(true);
     try {
+
+
+
       console.log(caption)
       const response = await axios.get(`http://localhost:5000/?prompt=${caption}`);
       console.log(response.data);
@@ -19,19 +23,43 @@ function App() {
     } finally {
       setLoading(false);
     }
+
   };
 
   return (
-    <div>
-      <h1 className="text-purple-500 justify-center">Piclingo</h1>
-      {loading ? <p>Loading...</p> : <img alt="OpenAI!" src={picture} />}
-      <input
-        onChange={(event) => {
+    
+    <div className="text-center bg-color relative bottom-0">
+      
+       <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">Visual.ai.d</a>
+        <a href="#contact">About</a>
+        <a href="https://github.com/joseph784/nwhack23">GitHub</a>
+      </div> 
+
+      <img src="logo.png" alt="Visual.ai.d"></img>
+      <div className="flex justify-center items-center">
+        <input className="w-1/4 mx-auto my-4"
+          onChange={(event) => {
           setCaption(event.target.value);
-        }}
-      />
-      <button onClick={handleClick}>Generate</button>
+         }}
+        />
+        <div>
+        <button  className="bg-purple-500 text-white p-2 rounded-lg" onClick={() => {handleClick(); 
+          var myPopup = document.getElementById("myPopup"); myPopup.classList.toggle("show");}}>Generate</button>
+          
+        </div>  
+          
+        {loading ? <p className="text-center text-gray-500 bg-color">Loading...</p> : null}
+      </div>
+
+      <div id="myPopup" className="popup">
+          <img src={picture} alt="Generated Image" />
+          <span onClick={() => {var myPopup = document.getElementById("myPopup"); myPopup.classList.toggle("show");}}>X</span>
+        </div>
+
     </div>
+    
   );
 }
 
