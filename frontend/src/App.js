b1204991468dba6f59e573c9bbf77611ffeb27f9
Aaron Chan <chan.aaron73@gmail.com>
2023-01-21 20:58:17 -0800

    fixed frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,3 @@
-// import "./App.css";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-
-// function App() {
-//   const [picture, setPicture] = useState("");
-
-//   useEffect(() => {
-//     console.log(picture);
-//   }, [picture]);
-
-//   return (
-//     <div>
-//       <h1 class="text-purple-500 justify-center">Piclingo</h1>
-//       <img alt="OpenAI!" src={picture} />
-//       <input />
-//       <button
-//         onClick={() => {
-//           axios.get("http://127.0.0.1:5000/").then(
-//             (response) => {
-//               console.log(response.data);
-//               setPicture(response.data);
-//             },
-//             (error) => {
-//               console.log(error);
-//             }
-//           );
-//         }}
-//       >
-//         Generate
-//       </button>
-//       <button
-//         onClick={() => {
-//           console.log(picture);
-//         }}
-//       >
-//         Test
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default App;
 import { useState } from "react";
 import axios from "axios";
 
@@ -63,7 +20,7 @@ function App() {
 
   return (
     <div>
-      <h1 class="text-purple-500 justify-center">Piclingo</h1>
+      <h1 className="text-purple-500 justify-center">Piclingo</h1>
       {loading ? (
         <p>Loading...</p>
       ) : (
